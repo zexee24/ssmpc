63a02adca2e00e600145fc8348347747aaeaa407
zexee24 <31950088+zexee24@users.noreply.github.com>
2023-02-27 17:22:36 +0200

    Skrewed around and found some things out

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::http::ContentType;
+use std::{fs, env};
+
+#[macro_use]extern crate rocket;
+
+#[get("/")]
+fn index() -> (ContentType, String){
+    (ContentType::HTML, String::from(fs::read_to_string("src/pages/index.html").expect("error opening file")))
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
