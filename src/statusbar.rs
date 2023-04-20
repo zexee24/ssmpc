ce93cb842825a66253d0424b8277236b8452c357
Jere Hasu <31950088+zexee24@users.noreply.github.com>
2023-04-20 15:01:46 +0300

    Added visible queue and fixed mobile

--- src/statusbar.rs
+++ src/statusbar.rs
@@ -1,5 +1,7 @@
+use std::time::Duration;
+
 use super::Psp;
-use ::yew::prelude::*;
+use yew::prelude::*;
 #[function_component]
 pub fn StatusBar(Psp { player_state }: &Psp) -> Html {
     if player_state.is_none() {
@@ -8,9 +10,28 @@ pub fn StatusBar(Psp { player_state }: &Psp) -> Html {
     let ps = player_state.as_ref().unwrap();
     match &ps.now_playing {
         Some(song) => {
-            html! {<p>{format!("Playing {:?} by {:?} at {:?}",song.name ,song.artist, ps.current_duration )}</p>
+            html! {
+                <div>
+                    <p>{format!("Playing {} by {} at {:?}",song.name ,song.artist.clone().unwrap_or("Unknown".to_string()), ps.current_duration.unwrap_or(Duration::from_secs(0)))}</p>
+                    <h2>{"Next up:"}</h2>
+                    <Queue player_state ={player_state.clone()}/>
+                </div>
             }
         }
         None => html! {<p>{"currently playing nothing"}</p>},
+    }
+}
+
+#[function_component]
+pub fn Queue(Psp { player_state }: &Psp) -> Html{
+    let name_list: Vec<_> = match player_state{
+        Some(p) => p.queue.clone().into(),
+        None => vec![],
+    };
+
+    html!{
+        <div>
+            {html!{name_list.iter().fold("".to_owned(), |acc, x| acc + &format!("{} | ", x.name))}}
+        </div>
     }
 }
